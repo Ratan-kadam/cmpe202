b738826dbb34632a92ae0aa06d13cfa34843721b
ratan kadam <ratan.kadam@gmail.com>
2014-09-16 04:04:44 -0700

    index updating

--- index.js
+++ index.js
@@ -37,18 +37,33 @@ function get(request, response) {
 
 function post(request, response) {
 	// TODO: read 'name and email from the request.body'
-	// var newSessionId = login.login('xxx', 'xxx@gmail.com');
+         
+	//var newSessionId = login.login('xxx', 'xxx@gmail.com');
 	// TODO: set new session id to the 'session_id' cookie in the response
 	// replace "Logged In" response with response.end(login.hello(newSessionId));
 
-	response.end("Logged In\n");
+       // TODO: read 'name and email from the request.body'
+	var newSessionId = login.login(request.body.name, request.body.email);
+
+	// TODO: set new session id to the 'session_id' cookie in the response
+	response.setHeader('Set-Cookie', 'session_id=' + newSessionId);
+
+	// replace "Logged In" response with response.end(login.hello(newSessionId));		
+	response.end(login.hello(newSessionId));
+
+//	response.end("Logged In..\n");
 };
 
 function del(request, response) {
-	console.log("DELETE:: Logout from the server");
+     //	console.log("DELETE:: Logout from the server");
  	// TODO: remove session id via login.logout(xxx)
  	// No need to set session id in the response cookies since you just logged out!
+        console.log("DELETE:: Logout from the server");
 
+ 	// TODO: remove session id via login.logout(xxx)
+ 	// No need to set session id in the response cookies since you just logged out!
+ 	var cookies = request.cookies;
+ 	login.logout(cookies['session_id']);
   	response.end('Logged out from the server\n');
 };
 
@@ -56,9 +71,15 @@ function put(request, response) {
 	console.log("PUT:: Re-generate new seesion_id for the same user");
 	// TODO: refresh session id; similar to the post() function
 
+   console.log("PUT:: Re-generate new session_id for the same user");
+	// TODO: refresh session id; similar to the post() function
+	var reNewed = login.login(request.body.name, request.body.email);
+	response.end("Re-freshed session id\n"+reNewed);
+
+
 	response.end("Re-freshed session id\n");
 };
 
-app.listen(8080);
+app.listen(8000);
 
 console.log("Node.JS server running at 8000...");
